60ad483bbb085ad30b83e29e2575d9c93be4f2ba
gameDNA <office@gamednastudio.com>
2019-02-20 00:19:26 +0000

    Update copyrights

--- Source/MultiDex/MultiDex.Build.cs
+++ Source/MultiDex/MultiDex.Build.cs
@@ -1,6 +1,6 @@
 // MultiDex
 // Created by Patryk Stepniewski & zompi.pl
-// Copyright (c) 2014-2018 gameDNA. All Rights Reserved.
+// Copyright (c) 2014-2019 gameDNA Ltd. All Rights Reserved.
 
 using System.IO;
 
